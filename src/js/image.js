a7ec586c8c0f74adb42fc9a96a854bf52bdc8521
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-04-23 11:15:45 +0300

    Update image.js

--- src/js/image.js
+++ src/js/image.js
@@ -6,16 +6,6 @@ const vegetables = import.meta.glob('/images/vegetables_img/*.png', {
 });
 console.log(vegetables);
 showVegetables(requestURL);
-// const requestURL = '/data/vegetables.json';
-
-// const request = new XMLHttpRequest();
-// request.open('GET', requestURL);
-// request.responseType = 'json';
-// request.send();
-// request.onload = function () {
-//   const vegetablesJSON = request.response;
-//   showVegetables(vegetablesJSON);
-// };
 
 function imageVegetable(name, price, image_url, retina_url) {
   let avifUrl = '';
@@ -36,9 +26,6 @@ function imageVegetable(name, price, image_url, retina_url) {
     if (imgFile === retina_url) {
       avifUrlRetina = images.sources['avif'].split(' ')[0];
       webpUrlRetina = images.sources['webp'].split(' ')[0];
-      //  imgBase = images.img['src'];
-      //  imgW = images.img['w'];
-      //  imgH = images.img['h'];
     }
   }
   const html = `<picture>
@@ -57,44 +44,16 @@ function imageVegetable(name, price, image_url, retina_url) {
 }
 
 function showVegetables(jsonObj) {
-  //   var section = document.querySelector('section');
   const vegetables = jsonObj;
   let vegetableHtml = '';
 
   for (var i = 0; i < vegetables.length; i++) {
-    //  console.log(vegetables[i].name);
-    //  console.log(vegetables[i].price);
-    //  console.log(vegetables[i].image_url);
     vegetableHtml += imageVegetable(
       vegetables[i].name,
       vegetables[i].price,
       vegetables[i].image_url,
       vegetables[i].retina_url
     );
-
-    console.log(vegetableHtml);
-    //  var myArticle = document.createElement('article');
-    //  var myH2 = document.createElement('h2');
-    //  var myPara1 = document.createElement('p');
-    //  var myPara2 = document.createElement('p');
-    //  var myPara3 = document.createElement('p');
-    //  var myList = document.createElement('ul');
-    //  myH2.textContent = heroes[i].name;
-    //  myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-    //  myPara2.textContent = 'Age: ' + heroes[i].age;
-    //  myPara3.textContent = 'Superpowers:';
-    //  var superPowers = heroes[i].powers;
-    //  for (var j = 0; j < superPowers.length; j++) {
-    //    var listItem = document.createElement('li');
-    //    listItem.textContent = superPowers[j];
-    //    myList.appendChild(listItem);
-    //  }
-    //  myArticle.appendChild(myH2);
-    //  myArticle.appendChild(myPara1);
-    //  myArticle.appendChild(myPara2);
-    //  myArticle.appendChild(myPara3);
-    //  myArticle.appendChild(myList);
-    //  section.appendChild(myArticle);
   }
   document.querySelector('#vegetablelist').innerHTML = `${vegetableHtml}`;
 }
